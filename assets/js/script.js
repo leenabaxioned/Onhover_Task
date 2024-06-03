d41991fe0aec7f7cc511fdc8924e6e69fed1d2a3
leenabaxioned <leenab@axioned.com>
2024-06-03 16:04:23 +0530

    content div positioning updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 /* Author: */
 var left = document.querySelector(".left");
 console.log(left);
+var leftContent = document.querySelector(".left-content");
 //left.style.background = "#6b63da"
 var right = document.querySelector(".right");
+var rightContent = document.querySelector(".right-content");
 //right.style.background = "#252525"
 
 left.addEventListener('mouseover', () => {
@@ -10,6 +12,8 @@ left.addEventListener('mouseover', () => {
   left.style.flexBasis = '100%';
   left.style.transition = "all 2s"
   right.style.flexBasis = '35%';
+  leftContent.style.left = "40%"
+  rightContent.style.right = "10%"
 });
 
 right.addEventListener('mouseover', () => {
@@ -17,4 +21,6 @@ right.addEventListener('mouseover', () => {
   right.style.flexBasis = '100%';
   right.style.transition = "all 2s"
   left.style.flexBasis = '35%';
+  rightContent.style.right = "40%"
+  leftContent.style.left = "10%"
 });
